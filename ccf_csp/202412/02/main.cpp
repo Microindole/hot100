045bb3a4dcd4b2927ca3307d5f83988bdd494ad0
Indolyn Yi <microindole@gmail.com>
2026-03-19 09:34:45 +0800

    加入代码格式规范

---
+++ ccf_csp/202412/02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n + 1, 0), b(n + 1, 0), w(n + 1, 0);
+
+    for (int i = 1; i < n + 1; i++) {
+        cout << w[i] << " ";
+    }
+    return 0;
+}
+/*
+i -> i+1:
+
+
+
+*/
