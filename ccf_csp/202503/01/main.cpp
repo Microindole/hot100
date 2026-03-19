96b06d152057f82cec014d98c5d4968dc6dd176a
Indolyn Yi <microindole@gmail.com>
2026-03-19 09:34:45 +0800

    加入代码格式规范

--- ccf_csp/202503/01/main.cpp
+++ ccf_csp/202503/01/main.cpp
@@ -1,19 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-int main(){
+int main() {
     int b, c, l, r;
-    cin>> b>> c>> l>> r;
+    cin >> b >> c >> l >> r;
     long long res = 0;
-    if(l % 2 != 0){
+    if (l % 2 != 0) {
         l += 1;
     }
 
-    while(l <= r){
-        res += 2 * (l*l +b*l +c);
+    while (l <= r) {
+        res += 2 * (l * l + b * l + c);
         l += 2;
     }
-    cout<<res<<endl;
+    cout << res << endl;
 }
